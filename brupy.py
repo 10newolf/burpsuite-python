618c72a7b447ff9e7a9206ca4fc8a31c09e37e82
10newolf <48347682+10newolf@users.noreply.github.com>
2021-05-05 11:26:24 +0530

    Add files via upload

---
+++ brupy.py
@@ -0,0 +1,27 @@
+import os
+
+os.system("clear")
+os.system("tar -xvf brupy.tar.gz")
+os.system("clear")
+print('''                                          
+ _____                _____     _ _       
+| __  |_ _ ___ ___   |   __|_ _|_| |_ ___ 
+| __ -| | |  _| . |  |__   | | | |  _| -_|
+|_____|___|_| |  _|  |_____|___|_|_| |___|
+              |_|     By Muhammad                                             
+''')
+
+os.system("pwd") #show Path
+os.system("java -jar burpsuite.jar")
+os.system("clear")
+print('''
+ _____                _____     _ _       
+| __  |_ _ ___ ___   |   __|_ _|_| |_ ___ 
+| __ -| | |  _| . |  |__   | | | |  _| -_|
+|_____|___|_| |  _|  |_____|___|_|_| |___|
+              |_|     By Muhammad                                
+''')
+os.system("clear")
+print('''
+ \033[1m\033[91m[!] This script must run as root\033[0m
+''')
